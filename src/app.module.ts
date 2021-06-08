f3f164e060cd934379ec3695db56be0092f64326
VincentAudibert <vincent.audib@gmail.com>
2021-06-08 11:44:49 +0200

    remove default app.stuff generated by nestjs init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PlaybookController } from './playbook/playbook.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, PlaybookController],
-  providers: [AppService],
+  controllers: [PlaybookController],
+  providers: [],
 })
 export class AppModule {}
